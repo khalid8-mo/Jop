1d04404f23e43a25a9be1108a7731c786130dfaa
khalid8-mo <aljokar014@gmail.com>
2021-01-15 14:28:20 +0200

    Send The Message And Lable Search

--- JopWebsite/Controllers/HomeController.cs
+++ JopWebsite/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using JopWebsite.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 using WebApplication1.Models;
@@ -9,12 +12,12 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
-      private  ApplicationDbContext db = new ApplicationDbContext();
+        private ApplicationDbContext db = new ApplicationDbContext();
 
         public ActionResult Index()
         {
-           
-            
+
+
             return View(db.Categories.ToList());
         }
 
@@ -27,7 +30,30 @@ namespace WebApplication1.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Contact(Contact contact)
+        {
+            //Send Message
+            var male = new MailMessage();
+            var loginInfo = new NetworkCredential("aljokar014@gmail.com","JR@13@jr!"); //Information your Email 
+            male.From = new MailAddress(contact.Email); // From Mail To LoginInfo
+            male.To.Add(new MailAddress("aljokar014@gmail.com"));
+            // Information The Message
+            male.Subject = "الموضوع : " + contact.Subject; 
+            var body = "اسم المرسل :"+contact.Name +"<br>"
+            + "عنوان المرسل : " + contact.Email + "<br>" +  
+            "عنوان الرساله : " + contact.Subject + "<br>"+
+            "نص الرساله  : " + contact.Massege;
+
+            male.Body = body;
+            var SmtpClientPort = new SmtpClient("asmp.gmail.com", 587); // Host And Port
+            SmtpClientPort.EnableSsl = true; // الرسايل التلقائيه
+            SmtpClientPort.Credentials = loginInfo; 
+            SmtpClientPort.Send(male); //Send The Message
 
             return View();
         }
@@ -39,14 +65,19 @@ namespace WebApplication1.Controllers
             Session["StudentNumber"] = db.Students.ToList().Count;
             return View();
         }
-        public ActionResult SerchForm()
+        public ActionResult Searsh()
         {
             return View();
         }
         [HttpPost]
-        public ActionResult SerchForm(string Search)
+        public ActionResult Searsh(string Search)
         {
-
+            var reuslt = db.Jops.Where(a => a.JopTitle.Contains(Search) ||
+            a.JopContent.Contains(Search) ||
+            a.Category.CategoryName.Contains(Search) ||
+            a.Category.CategoryDescription.Contains(Search)
+            
+            );
             return View();
         }
     }
---
+++ JopWebsite/Models/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace JopWebsite.Models
+{
+    public class Contact
+    {
+        [Required]
+        [Display(Name="الاسم")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name="الموضوع")]
+        public string Subject { get; set; }
+        [Required]
+        [Display(Name = "الرساله")]
+
+        public string Massege { get; set; }
+        [Required]
+        [Display(Name = "البريد الالكتروني")]
+
+        public string Email { get; set; }
+    }
+}
