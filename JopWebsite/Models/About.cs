60009bfeded69dde595819556367689c7686f049
khalid8-mo <aljokar014@gmail.com>
2021-01-15 15:08:27 +0200

    A

---
+++ JopWebsite/Models/About.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JopWebsite.Models
+{
+    public class About
+    {
+        public string Name { get; set; }
+        public string Discraption { get; set; }
+        public string Image { get; set; }
+
+        internal object ToList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
